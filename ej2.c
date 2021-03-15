3b47f71f01f01ba885b2902c5a0a0655f1f14730
Acrux152 <72485442+Acrux152@users.noreply.github.com>
2021-03-14 20:52:34 -0600

    Create ej2.c

---
+++ ej2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Equipo 9. Código escrito por Novak Okcham González*/
+
+int main(){
+    char a[3][3];
+    int i, j;
+
+    for(i=0; i<3; i++){        
+        for(j=0; j<3; j++){            /*Ciclo para pedirle los datos al usuario*/
+            printf("Introduzca los valores del arreglo en %i %i \n", i, j);
+            scanf("%c", &a[i][j]);
+            fflush(stdin);                          /*Importante para que guarde bien los caracteres*/
+        }
+    }
+
+    for(i=0; i<3; i++){        
+        for(j=0; j<3; j++){            /*Ciclo para pedirle los datos al usuario*/
+            printf("Valor almacenado en %i %i: %c\nDirecci%cn de memoria: %p\n", i, j,  a[i][j], 162, &a[i][j]);            /*Importante para que guarde bien los caracteres*/
+        }
+    }
+
+    printf("\nDireccion de memoria del primer elemento del arreglo: %p\n", &a[0][0]);
+    printf("Direccion de memoria de la variable de tipo arreglo arr: %p\n", &a);
+
+    system("pause");
+    return 0;
+}
