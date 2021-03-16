c4aa8643cc337e04b952ad29d1826ba594a6283f
Acrux152 <72485442+Acrux152@users.noreply.github.com>
2021-03-16 00:18:49 -0600

    Update ej3.c

--- ej3.c
+++ ej3.c
@@ -3,11 +3,20 @@
 
 /*Equipo 9. Código escrito por Novak Okcham González*/
 
+/*13 DE MARZO DE 2021*/
+
+/*Variables usadas en los ciclos declaradas de manera global para evitar incompatibilidades de IDE*/
+
+int i, j, k;
+
 int main(){
+    
     char a[3][2][4];
+    
     int i, j, k;
 
-    for(i=0; i<3; i++){      /*Ciclo para pedirle los datos al usuario*/  
+    /*Ciclo para pedirle los datos al usuario*/
+    for(i=0; i<3; i++){        
         for(j=0; j<2; j++){            
             for(k=0; k<4; k++){
                 printf("Introduzca los valores del arreglo en %i %i %i \n", i, j, k);
@@ -16,12 +25,14 @@ int main(){
             }
         }
     }
-
-    for(i=0; i<3; i++){         /*imprime las direcciones de memoria del arreglo*/
+    
+    /*Ciclo para imprimir la matriz 3x2x4*/
+    for(i=0; i<3; i++){
         printf("\nMatriz %d\n", i+1);     
-        for(j=0; j<2; j++){          
+        for(j=0; j<2; j++){ 
+            printf("|\t");              
             for(k=0; k<4; k++){
-                printf("%p\t",  &a[i][j][k]);
+                printf("%p\t|\t",  &a[i][j][k]);
             }
             printf("\n");
         }
@@ -29,7 +40,7 @@ int main(){
     
     printf("\nDireccion de memoria del primer elemento del arreglo: %p\n", &a[0][0][0]);
     printf("Direccion de memoria de la variable de tipo arreglo arr: %p\n", &a);
-
+    
     system("pause");
     return 0;
 }
