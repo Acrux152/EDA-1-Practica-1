f233b2f8e1f65268d188b84b4bf6879ad32dc773
Acrux152 <72485442+Acrux152@users.noreply.github.com>
2021-03-16 00:10:43 -0600

    Update ej3.c

--- ej3.c
+++ ej3.c
@@ -6,11 +6,9 @@
 int main(){
     char a[3][2][4];
     int i, j, k;
-    int * p;
-    p=&a;
 
-    for(i=0; i<3; i++){        
-        for(j=0; j<2; j++){            /*Ciclo para pedirle los datos al usuario*/
+    for(i=0; i<3; i++){      /*Ciclo para pedirle los datos al usuario*/  
+        for(j=0; j<2; j++){            
             for(k=0; k<4; k++){
                 printf("Introduzca los valores del arreglo en %i %i %i \n", i, j, k);
                 scanf("%c", &a[i][j][k]);
@@ -19,18 +17,18 @@ int main(){
         }
     }
 
-    for(i=0; i<3; i++){   
+    for(i=0; i<3; i++){         /*imprime las direcciones de memoria del arreglo*/
         printf("\nMatriz %d\n", i+1);     
-        for(j=0; j<2; j++){            /*Ciclo para pedirle los datos al usuario*/
+        for(j=0; j<2; j++){          
             for(k=0; k<4; k++){
-                printf("%p\t",  &a[i][j][k]);            /*Importante para que guarde bien los caracteres*/
+                printf("%p\t",  &a[i][j][k]);
             }
             printf("\n");
         }
     }
     
     printf("\nDireccion de memoria del primer elemento del arreglo: %p\n", &a[0][0][0]);
-    printf("Direccion de memoria de la variable de tipo arreglo arr: %p\n", p);
+    printf("Direccion de memoria de la variable de tipo arreglo arr: %p\n", &a);
 
     system("pause");
     return 0;
